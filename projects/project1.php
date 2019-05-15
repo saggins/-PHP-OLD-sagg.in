513fb63f39d0f736988a92055976757eb0ef551a
Sangmin Chun <win32prog@gmail.com>
2019-05-14 20:27:55 -0700

    added inital project

--- projects/project1.php
+++ projects/project1.php
@@ -1,3 +1,9 @@
+<?php
+    include("/config/sql_config.php");
+    session_start();
+
+?>
+
 <html>
     <title>anime livestream project</title>
     <head>
@@ -5,14 +11,15 @@
     </head>
     
     <body>
-
-    
+        <div>
+            <form action="" method="post">
+                <label>Code: </label><input type="text" name="code" class="box"><br>
+                <input type="submit" value=" Submit ">
+        </div>
 
     </body>
 </html>
 
 <?php
-
-
-
+    
 ?>
