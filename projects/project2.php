59a9288a9c92038ac9e38881f19510cc7a3d7dcb
win32prog <win32prog@gmail.com>
2019-05-23 23:23:00 -0700

    Added Inital Html and Php

---
+++ projects/project2.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Server Worker... yeah</title>
+    </head>
+
+    <body>
+        <form action="POST">
+            <p>Which game m8</p>
+            <!-- Add games like Tf2, CSgo, IDFK -->
+            <input type="radio" name="game" value="minecraft" checked> Custom Minecraft <br>
+            <input type="radio" name="game" value="VanilaServer"> Saggins' Orignal Vanila Server <br>
+
+            <!-- Just incase -->
+            <p>Forge?</p>
+
+            <p>Server Properties</p>
+            <!-- Css: Make text book biger -->
+            <input type="text" name="Server Properties" value=""><br>
+
+            <p>Custom Map</p>
+            <input type="file" name="Map" value=""> <br>
+            <p>Op(Player name If more pls seperate with commas)</p>
+            <input type="text" name="Op" value=""><br>
+
+            <input type="submit">
+
+
+        </form>
+    </body>
+</html>
+
+<?php
+
+
+
+?>
