7c2c5a1500c4c3a7bba89617f5c69e1ecd5d98dc
win32prog <win32prog@gmail.com>
2019-05-25 19:23:34 -0700

    Added RndomCode gen

---
+++ projects/project1S.php
@@ -0,0 +1,15 @@
+<?php
+include("RandomCode.php");
+session_start();
+
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="">
+            <label>Upload vid here bruh</label><input type="file">
+        </form>
+    </body>
+</html>
