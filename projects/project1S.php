b5cf65478fdc65808f1f08916962c2f939410aad
win32prog <win32prog@gmail.com>
2019-06-13 23:00:24 -0700

    RandomCodeGenIThink

--- projects/project1S.php
+++ projects/project1S.php
@@ -10,6 +10,14 @@ session_start();
     <body>
         <form action="">
             <label>Upload vid here bruh</label><input type="file">
+            <input type="submit">
         </form>
     </body>
 </html>
+
+<?php
+
+    $code4user = RandomCode(10);
+    echo($code4user);
+
+?>
