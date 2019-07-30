53d8ee6f20a428f406869634fd87bc58e86b0f04
win32prog <win32prog@gmail.com>
2019-07-30 16:18:39 -0700

    attempt at vuejs idkf

--- projects/project2.php
+++ projects/project2.php
@@ -1,36 +1,41 @@
 <html>
     <head>
         <title>Server Worker... yeah</title>
+        <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+        <script src="project2.js"></script>
     </head>
 
     <body>
         <form action="POST">
-            <p>Which game m8</p>
-            <!-- Add games like Tf2, CSgo, IDFK -->
-            <input type="radio" name="game" value="minecraft" checked> Custom Minecraft <br>
-            <input type="radio" name="game" value="VanilaServer"> Saggins' Orignal Vanila Server <br>
+            <div id="SB"> <!-- SB = Server Browser -->
+                <p>Which game m8</p>
+                    <!-- Add games like Tf2, CSgo, IDFK -->
+                    <input type="radio" name="game" id="cusmc:" value="minecraft" checked v-model="server"> Custom Minecraft <br>
+                    <input type="radio" name="game" id="valmc" value="VanilaServer" v-model="server"> Saggins' Orignal Vanila Server <br>
+                    <input type="radio" name="game" id="source" value="TF2" v-model="server"> TF2 <br>
+                <div v-if="server == 'cusmc'">
+                    <p>Forge?</p>
+                </div>
+                    
+                <div v-if="server = 'cusmc'">
+                    <p>Custom Map</p>
+                        <input type="file" name="Map" value=""> <br>
+                    <p>Op(Player name If more pls seperate with commas)</p>
+                        <input type="text" name="Op" value=""><br>
+                </div>
+                <div v-if="server == 'cusmc' || 'valmc'">
+                    <p>Server Properties </p>
+                    <!-- Css: Make text book biger -->
+                    <input type="text" name="ServerProperties" value=""><br>
+                </div>
+             <input type="submit">
 
-            <!-- Just incase -->
-            <p>Forge?</p>
-
-            <p>Server Properties</p>
-            <!-- Css: Make text book biger -->
-            <input type="text" name="ServerProperties" value=""><br>
-
-            <p>Custom Map</p>
-            <input type="file" name="Map" value=""> <br>
-            <p>Op(Player name If more pls seperate with commas)</p>
-            <input type="text" name="Op" value=""><br>
-
-            <input type="submit">
-
-
+            </div>
         </form>
     </body>
 </html>
 
 <?php
 
-file_put_contents("./project2/server.properties", escapeshellarg( $_POST["ServerProperties"] ) )
 
 ?>
