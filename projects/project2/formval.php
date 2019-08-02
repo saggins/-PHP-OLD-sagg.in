05fb445350fc70b9bf92e0fac8bfaf87ca98abd9
win32prog <win32prog@gmail.com>
2019-08-02 15:07:24 -0700

    Started work on PHP back bone

---
+++ projects/project2/formval.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This is a php file that contains the main class for Form Valiadtion. 
+ * This should also vaildate if it has any security things to worry about. If any are detctable.
+ * 
+ * @author Sangmin Chun <win32prog@gamil.com>
+ * 
+ */
+
+class FormValidate
+{
+    /**
+     * Will return a boolean Variable called vaildate
+     * 
+     * It'll say if the file is good to go for server
+     * 
+     * __construct should have the file path or $file, 
+     * 
+     * validate should be extended for specific games or game genres
+     *  EX: Source games will have diffrent cases than Minecraft Games
+     *      Addtionally Minecraft will have diffrent varients such as the Vailna Server and Custom Server
+     * 
+     * 
+     * NOTE: Continue with https://www.killerphp.com/tutorials/php-objects-page-3/
+     * 
+     */
+
+    public $file;
+    
+    public function __construct($file, )
+    {
+        $this 
+    }
+    public function validente() 
+    {
+
+    }
+}
+?>
--- projects/project2/project2.php
+++ projects/project2/project2.php
@@ -1,59 +1,14 @@
 <html>
     <head>
-        <title>Server Worker... yeah</title>
-        <script src="vue.min.js"></script>
+        <title>Server Is <?php echo $success?> </title>
     </head>
-
     <body>
-        <form action="POST">
-            <div id="SB"> <!-- SB = Server Browser -->
-                <p>Which game m8</p>
-                    <!-- Add games like Tf2, CSgo, IDFK -->
-                    <ol>
-                        <li> 
-                            <input type="radio" name="game" id="cusmc" value="cusmc" checked v-model="server">
-                            <label for="cusmc"> Custom Minecraft </label>
-                        </li>
-
-                        <li>
-                            <input type="radio" name="game" id="valmc" value="valmc" v-model="server">
-                            <label for="valmc"> Saggins' Orignal Vanila Server </label>
-                        </li>
-
-                        <li>
-                            <input type="radio" name="game" id="source" value="source" v-model="server">
-                            <label for="TF2">TF2</label>  
-                        </li>
-                    </ol>
-                <template v-if="server === 'cusmc'">
-                    <p>Forge?</p>
-                </template>
-                    
-                <template v-if="server === 'cusmc'">
-                    <p>Custom Map</p>
-                        <input type="file" name="Map" value=""> <br>
-                    <p>Op(Player name If more pls seperate with commas)</p>
-                        <input type="text" name="Op" value=""><br>
-                </template>
-                <template v-if="server === 'cusmc' || server === 'valmc'">
-                    <p>Server Properties </p>
-                    <!-- Css: Make text book biger -->
-                    <input type="text" name="ServerProperties" value=""><br>
-                </template>
-
-                <template v-if="server === 'source'">
-                    <p>Scripts</p>    
-                </template>
-             <input type="submit">
-
-            </div>
-        </form>
-        <script src="project2.js"></script>
-
+        
     </body>
 </html>
 
-<?php
 
+<?php
+    include 'formval.php';
 
 ?>
