92da9576f9c3830bd4f2bf468de0f69e5895f5fe
win32prog <win32prog@gmail.com>
2019-08-13 18:41:19 -0700

    did stuff

--- projects/project2/project2.php
+++ projects/project2/project2.php
@@ -1,14 +1,19 @@
+
 <html>
     <head>
         <title>Server Is <?php echo $success?> </title>
     </head>
     <body>
-        
+        Which Server:<?php echo $server?> 
     </body>
 </html>
-
 
 <?php
     require 'lib/formval.php';
     require 'lib/runserver.php';
+
+
+    $server= $_POST["game"];
+    phpinfo();
 ?>
+
