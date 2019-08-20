77ac77221f9a3ae4804771ff1c3e478d21ab5d46
win32prog <win32prog@gmail.com>
2019-08-19 19:13:12 -0700

    Added Todos' and checkserver lib

--- projects/project2/project2.php
+++ projects/project2/project2.php
@@ -18,4 +18,9 @@
     $server = new RunSagsMC("/home/saggins/Documents/projects/minecraft-test", "startserver.sh");
     $server->start();
 
+
+
+
+    # TODO: Make a system where if theres noone online than kill the server  GOTO:https://github.com/xPaw/PHP-Minecraft-Query
+    # TODO: Make a system where if people retry this url it won't make mutliple servers
 ?>
